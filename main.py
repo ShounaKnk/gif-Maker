b847b2b90fb281652217b01886d07b6a037926bb
ShounaKnk <shounakkannaik0607@gmail.com>
2025-03-17 00:46:41 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import imageio.v3 as iio
+
+files =[
+    'im0.jpg',
+    'im1.jpg',
+    'im2.jpg',
+    'im3.jpg',
+    'im4.jpg',
+    'im5.jpg',
+    'im6.jpg',
+    'im7.jpg',
+    'im8.jpg',
+    'im9.jpg',
+    'im10.jpg'
+]
+
+images =[]
+
+for image in files:
+    images.append(iio.imread(image))
+
+iio.imwrite('Gif_name.gif',
+images, duration = 100, loop =0)
